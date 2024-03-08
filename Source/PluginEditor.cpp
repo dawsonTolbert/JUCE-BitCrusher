1e53615fb162ca863a5b65047aa4958d3e20a1ee
dawsonTolbert <tolbert_dawson@yahoo.com>
2024-03-08 14:43:15 -0500

    Added Crush Slider to GUI

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,6 +16,14 @@ BitCrusherAudioProcessorEditor::BitCrusherAudioProcessorEditor (BitCrusherAudioP
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+
+    crushSlider.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+    crushSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 100, 25);
+    crushSlider.setRange(0.0, 10.0);
+    crushSlider.setValue(0.0);
+    //crushSlider.addListener(this);
+
+    addAndMakeVisible(crushSlider);
 }
 
 BitCrusherAudioProcessorEditor::~BitCrusherAudioProcessorEditor()
@@ -27,14 +35,11 @@ void BitCrusherAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("BitCrusher", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void BitCrusherAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    crushSlider.setBounds(getLocalBounds());
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -29,5 +29,7 @@ private:
     // access the processor object that created it.
     BitCrusherAudioProcessor& audioProcessor;
 
+    juce::Slider crushSlider;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BitCrusherAudioProcessorEditor)
 };
